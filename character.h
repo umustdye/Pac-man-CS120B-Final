7677aa2278a6dbb3c2bceca8758870c1101a201d
umustdye <43763861+umustdye@users.noreply.github.com>
2021-12-07 06:28:58 -0800

    fixed collisions and updating boarder box

--- character.h
+++ character.h
@@ -82,21 +82,41 @@ typedef struct Character{
 	int back_tile_x; //column
 	int back_tile_y; //row
 	//pixel values for the boarder box for drawing and collisions
-	unsigned char boarder_x[4]; //TL -> TR -> BR -> BL
-	unsigned char boarder_y[4]; //TL -> TR -> BR ->BL
+	int boarder_x[4]; //TL -> TR -> BR -> BL
+	int boarder_y[4]; //TL -> TR -> BR ->BL
 	unsigned char direct; //can be 'U', 'R', 'D', 'L', or 'I'
 	int index; //index for animation frame
 	int max_index; //how many frames in animation cycle
 	//previous location for graphics
-	unsigned char prev_boarder_x[4];
-	unsigned char prev_boarder_y[4];
-	unsigned char target_tile_x; //for ghosts
-	unsigned char target_tile_y; //for ghosts
+	int prev_boarder_x[4];
+	int prev_boarder_y[4];
+	int target_tile_x; //for ghosts
+	int target_tile_y; //for ghosts
 
 
 } Character;
 
+void initialize_character(Character *character, 	
+	int back_tile_x, int back_tile_y, int boarder_x[4], int boarder_y[4], unsigned char direct,
+	int index, int max_index, int prev_boarder_x[4], int prev_boarder_y[4], int target_tile_x,
+	int target_tile_y)
+{
+	character->back_tile_x = back_tile_x;
+	character->back_tile_y = back_tile_y;
+	for(int i=0; i<4; i++)
+	{
+		character->boarder_x[i] = boarder_x[i];
+		character->boarder_y[i] = boarder_y[i];
+		character->prev_boarder_x[i] = prev_boarder_x[i];
+		character->prev_boarder_y[i] = prev_boarder_y[i];
+	}
 
+	character->direct = direct;
+	character->index = index;
+	character->max_index = max_index;
+	character->target_tile_x = target_tile_x;
+	character->target_tile_y = target_tile_y;
+}
 
 void prepare_send_data(Character character, unsigned char data[])
 {
@@ -107,54 +127,76 @@ void prepare_send_data(Character character, unsigned char data[])
 //get the upper tile for graphics
 unsigned char tile_graphics_x(Character character)
 {
-	return character.boarder_x[0]/8;
+	return (unsigned char)(character.boarder_x[0]/8);
 }
 
 //get the upper tile offset for graphics
 unsigned char tile_offset_graphics_x(Character character)
 {
-	return character.boarder_x[0]%8;
+	return (unsigned char)(character.boarder_x[0]%8);
 }
 
 //get the upper tile for graphics
 unsigned char tile_graphics_y(Character character)
 {
-	return character.boarder_y[0]/8;
+	return (unsigned char)(character.boarder_y[0]/8);
 }
 
 //get the upper tile offset for graphics
 unsigned char tile_offset_graphics_y(Character character)
 {
-	return character.boarder_y[0]%8;
+	return (unsigned char)(character.boarder_y[0]%8);
 }
 
 //get the upper tile for graphics
 unsigned char prev_tile_graphics_x(Character character)
 {
-	return character.prev_boarder_x[0]/8;
+	return (unsigned char)(character.prev_boarder_x[0]/8);
 }
 
 //get the upper tile offset for graphics
 unsigned char prev_tile_offset_graphics_x(Character character)
 {
-	return character.prev_boarder_x[0]%8;
+	return (unsigned char)(character.prev_boarder_x[0]%8);
 }
 
 //get the upper tile for graphics
 unsigned char prev_tile_graphics_y(Character character)
 {
-	return character.prev_boarder_y[0]/8;
+	return (unsigned char)(character.prev_boarder_y[0]/8);
 }
 
 //get the upper tile offset for graphics
 unsigned char prev_tile_offset_graphics_y(Character character)
 {
-	return character.prev_boarder_y[0]%8;
+	return (unsigned char)(character.prev_boarder_y[0]%8);
 }
 
 //check for map collisions had to keep it separate for the ghosts
-unsigned char map_collision(int dist, Character *character){
-	unsigned char new_location = game_map[character->back_tile_y + dist][character->back_tile_x + dist];
+unsigned char map_collision(int dist, unsigned char direct, Character *character){
+	unsigned char new_location;
+	switch (direct)
+	{
+	case 'U':
+		new_location = game_map[character->back_tile_y + dist][character->back_tile_x];
+		break;
+
+	case 'R':
+		new_location = game_map[character->back_tile_y][character->back_tile_x + dist];
+		break;
+
+	case 'D':
+		new_location = game_map[character->back_tile_y + dist][character->back_tile_x];
+		break;
+	
+	case 'L':
+		new_location = game_map[character->back_tile_y][character->back_tile_x + dist];
+		break;
+	default:
+		new_location = game_map[character->back_tile_y][character->back_tile_x];
+		break;
+	}
+	
 	printf("%c", new_location);
 	return new_location;
 }
@@ -199,17 +241,44 @@ void update_box(int dist, unsigned char direct, Character *character)
 	{
 		character->prev_boarder_y[i] = character->boarder_y[i];
 	}
-
-	//set new values as current		
-	for(int i=0; i<4; i++)
+	//future heidi change this, it's moving both dimensions at once which is wrong
+	//set new values as current	
+	switch (direct)
 	{
-		character->boarder_x[i] = (unsigned char)((int)(character->boarder_x[i]) + (dist*8));
-	}
+	case 'U':
+		for(int i=0; i<4; i++)
+		{
+			character->boarder_y[i] = ((character->boarder_y[i]) + (dist*8));
+		}
+		break;
 
-	for(int i=0; i<4; i++)
-	{
-		character->boarder_y[i] = (unsigned char)((int)(character->boarder_y[i]) + (dist*8));
-	}
+	case 'R':
+		for(int i=0; i<4; i++)
+		{
+			character->boarder_x[i] = ((character->boarder_x[i]) + (dist*8));
+		}
+
+		break;
+
+	case 'D':
+		for(int i=0; i<4; i++)
+		{
+			character->boarder_y[i] = ((character->boarder_y[i]) + (dist*8));
+		}
+		break;
+
+	case 'L':
+		for(int i=0; i<4; i++)
+		{
+			character->boarder_x[i] = ((character->boarder_x[i]) + (dist*8));
+		}
+
+		break;
+	
+	default:
+		break;
+	}	
+
 
 
 
@@ -226,9 +295,6 @@ void update_box(int dist, unsigned char direct, Character *character)
 	update_back_tile(dist, direct, character);
 	
 }
-
-
-
 
 
 
--- test.c
+++ test.c
@@ -4,17 +4,54 @@
 int main() {
 
     initiate_map();
-    //draw_map();
+    draw_map();
 
     //Test collision function
     Character test_man;
-    test_man.back_tile_x = 11;
-    test_man.back_tile_y = 1;
-    unsigned char test_collision = map_collision(2, &test_man);
+    int back_tile_x = 11; //column
+	int back_tile_y = 1; //row
+	//pixel values for the boarder box for drawing and collisions
+	int boarder_x[4]; //TL -> TR -> BR -> BL
+    boarder_x[0] = back_tile_x * 8;
+    boarder_x[1] = boarder_x[0] + 16;
+    boarder_x[2] = boarder_x[1];
+    boarder_x[3] = boarder_x[0]; 
+	int boarder_y[4]; //TL -> TR -> BR ->BL
+    boarder_y[0] = back_tile_y * 8 - 4;
+    boarder_y[1] = boarder_y[0];
+    boarder_y[2] = boarder_y[0] - 16;
+    boarder_y[3] = boarder_y[2];
+	unsigned char direct = 'R'; //can be 'U', 'R', 'D', 'L', or 'I'
+	int index = 0; //index for animation frame
+	int max_index = 4; //how many frames in animation cycle
+	//previous location for graphics
+	int prev_boarder_x[4];
+    prev_boarder_x[0] = back_tile_x * 8;
+    prev_boarder_x[1] = prev_boarder_x[0] + 16;
+    prev_boarder_x[2] = prev_boarder_x[1];
+    prev_boarder_x[3] = prev_boarder_x[0]; 
+	int prev_boarder_y[4];
+    prev_boarder_y[0] = back_tile_y * 8 - 4;
+    prev_boarder_y[1] = prev_boarder_y[0];
+    prev_boarder_y[2] = prev_boarder_y[0] - 16;
+    prev_boarder_y[3] = prev_boarder_y[2];
+	int target_tile_x = 0; //for ghosts
+	int target_tile_y = 0; //for ghosts
+    initialize_character(&test_man, back_tile_x, back_tile_y, boarder_x, boarder_y, direct, index, max_index, prev_boarder_x, prev_boarder_y, target_tile_x, target_tile_y);
+
+    int dist = -1;
+    unsigned char direction = 'L';
+    unsigned char test_collision = map_collision(dist, direct, &test_man);
 
     if(test_collision == 'W')
     {
         printf("\nCollision");
+    }
+
+    else
+    {
+        //update the postion
+        update_position(dist, direction, &test_man);
     }
 
     return 0;
